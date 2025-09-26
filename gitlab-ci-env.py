59e9846a866f9f91c25767257897ddd76cfbfe1d
steinuil <steenuil.owl@gmail.com>
2025-09-26 03:27:23 +0200

    fix some bugs, add environment URL flag

--- gitlab-ci-env.py
+++ gitlab-ci-env.py
@@ -3,7 +3,7 @@
 import hashlib
 import re
 from dataclasses import dataclass, asdict
-from typing import Sequence
+from typing import Optional, Sequence
 
 
 BASE36_LOWERCASE_ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyz"
@@ -29,12 +29,28 @@ def b36encode(b: bytes, alphabet: Sequence[str] = BASE36_LOWERCASE_ALPHABET) -> 
 # | sed -E 's/[^a-z0-9-]+/-/g'
 # | sed -E 's/^-*([a-z0-9-]+[a-z0-9])-*$$/\1/g'
 def generate_commit_ref_slug(branch: str) -> str:
-    return re.sub("[^a-z0-9-]+", "-", branch.lower()[:63]).strip("-")
+    """
+    Generate the CI_COMMIT_REF_SLUG env variable used by GitLab CI from the branch name.
+    """
+
+    slug = branch.lower()[:63]
+    slug = re.sub("[^a-z0-9-]+", "-", slug)
+
+    # Unfortunately we can't just call .strip("-")
+    # because that wouldn't work with e.g. "a-" and we should be
+    # bug-for-bug compatible with the shell script.
+    slug = re.sub("^-*([a-z0-9-]+[a-z0-9])-*$$", "\\1", slug)
+
+    return slug
 
 
 # Reference in GitLab's codebase:
 # https://gitlab.com/gitlab-org/gitlab/-/blob/e9b70ffa9204e57c1a89046d9e591aa16873e661/lib/gitlab/slug/environment.rb
 def generate_environment_slug(environment_name: str) -> str:
+    """
+    Generate the CI_ENVIRONMENT_SLUG env variable used by GitLab CI from an environment name.
+    """
+
     # Turn to lowercase and replace all non-alphanumeric chars
     # with a dash
     slug = re.sub("[^a-z0-9]", "-", environment_name.lower())
@@ -46,31 +62,29 @@ def generate_environment_slug(environment_name: str) -> str:
     # Collapse multiple consecutive dashes
     slug = re.sub(r"\-+", "-", slug)
 
-    # If the environment name was already a valid slug and it is shorter
-    # than 24 characters, leave it as is
+    # If the environment name was already a valid slug and it is shorter than 24 characters,
+    # just remove trailing dashes and return as is.
     if len(slug) <= 24 and slug == environment_name:
-        return slug
+        return slug.rstrip("-")
 
-    # Truncate the slug to 16 characters and make sure it ends
-    # with a dash
-    slug = slug[:16]
+    # Truncate the slug to 17 characters and make sure it ends with a dash.
+    # Slightly confusing in the Ruby implementation because the 0..16 range is inclusive!
+    slug = slug[:17]
     if not slug.endswith("-"):
         slug += "-"
 
-    # Generate the SHA256 hash of the environment name and convert it
-    # to base36.
+    # Generate the SHA256 hash of the environment name and convert it to base36.
     hash = hashlib.sha256(environment_name.encode()).digest()
     base36_hash = b36encode(hash)
 
-    # Concatenate the slug to the last 6 characters
+    # Concatenate the slug to the last 6 characters of the hash.
     return slug + base36_hash[-6:]
 
 
 def interpolate_env_variables(string: str, env_variables: dict[str, str]) -> str:
-    # env_variables = {
-    #     "CI_COMMIT_REF_NAME": branch_name,
-    #     "CI_COMMIT_REF_SLUG": commit_ref_slug,
-    # }
+    """
+    Interpolate env variables prefixed by $ or included within ${} in a string.
+    """
 
     for key, val in env_variables.items():
         string = string.replace(f"${key}", val).replace(f"${{{key}}}", val)
@@ -84,30 +98,43 @@ class PredefinedVariables:
     CI_COMMIT_REF_SLUG: str
     CI_ENVIRONMENT_NAME: str
     CI_ENVIRONMENT_SLUG: str
+    CI_ENVIRONMENT_URL: Optional[str] = None
 
     @staticmethod
     def generate(
-        branch: str, environment_name: str, env_variables: dict[str, str] = {}
+        *,
+        branch: str,
+        environment_name: str,
+        environment_url: Optional[str] = None,
+        env: dict[str, str] = {},
     ) -> "PredefinedVariables":
         commit_ref_slug = generate_commit_ref_slug(branch)
 
-        environment_name = interpolate_env_variables(
-            environment_name,
-            env_variables={
-                **env_variables,
-                "CI_COMMIT_REF_NAME": branch,
-                "CI_COMMIT_REF_SLUG": commit_ref_slug,
-            },
+        predefined = {
+            "CI_COMMIT_REF_NAME": branch,
+            "CI_COMMIT_REF_SLUG": commit_ref_slug,
+        }
+
+        ci_environment_name = interpolate_env_variables(environment_name, predefined)
+
+        environment_slug = generate_environment_slug(ci_environment_name)
+
+        predefined.update(
+            {
+                "CI_ENVIRONMENT_NAME": ci_environment_name,
+                "CI_ENVIRONMENT_SLUG": environment_slug,
+            }
         )
 
-        environment_slug = generate_environment_slug(environment_name)
+        if environment_url:
+            url = interpolate_env_variables(
+                environment_url,
+                {**env, **predefined},
+            )
 
-        return PredefinedVariables(
-            CI_COMMIT_REF_NAME=branch,
-            CI_COMMIT_REF_SLUG=commit_ref_slug,
-            CI_ENVIRONMENT_NAME=environment_name,
-            CI_ENVIRONMENT_SLUG=environment_slug,
-        )
+            predefined.update({"CI_ENVIRONMENT_URL": url})
+
+        return PredefinedVariables(**predefined)
 
 
 def _cli_parser():
@@ -135,6 +162,12 @@ gitlab-ci-env --branch TEST-branch --environment-name 'deployment-$CI_COMMIT_REF
         required=True,
         type=str,
     )
+    parser.add_argument(
+        "--environment-url",
+        help="Environment URL of the CI step from the .gitlab-ci.yml configuration.",
+        required=False,
+        type=str,
+    )
 
     return parser
 
@@ -145,6 +178,7 @@ if __name__ == "__main__":
     predefined_variables = PredefinedVariables.generate(
         branch=args.branch,
         environment_name=args.environment_name,
+        environment_url=args.environment_url,
     )
 
     import json
